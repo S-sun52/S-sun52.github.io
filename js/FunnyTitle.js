3ded72d93810b3a8774009340a54a8132e569c14
S-sun52 <2836374403@qq.com>
2020-02-17 20:54:43 +0800

    Site updated: 2020-02-17 20:54:43

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -5,12 +5,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         document.title = '你丑你就走！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         document.title = '我想你了' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
